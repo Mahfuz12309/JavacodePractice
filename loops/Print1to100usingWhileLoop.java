c7ead5980f03f008684075054dd1bef553061202
Mahfuz12309 <mahfuz15-2713@diu.edu.bd>
2024-03-05 23:23:26 +0600

    a program to print 1 to 100 using while loop

---
+++ loops/Print1to100usingWhileLoop.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class Print1to100usingWhileLoop {
+    public static void main(String[] args) {
+        int i=1;
+        while(i<=100)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
