f9822a787d3944e1222c42e4912be4d147456ceb
Mahfuz <mahfuz15-2713@diu.edu,bd>
2024-03-05 21:54:21 +0600

    calculatorr.java added

--- condition/calculator.java
+++ condition/calculator.java
@@ -3,6 +3,39 @@ public class calculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float a, b, ans;
+        char choice;
+        System.out.print("Enter two number you want to calculate: ");
+        a=sc.nextFloat();
+        System.out.print("Enter operation sign you want to calculate: ");
+        b= sc.nextFloat();
+        choice = sc.next().charAt(0);
+        switch (choice) {
+            case '+':{
+                ans = a+b;
+                System.out.println(a+"+"+b+"="+ans);
+                break;
+            }
+            case '-':{
+                ans = a-b;
+                System.out.println(a+"-"+b+"="+ans);
+                break;
+            }
+            case '*':{
+                ans = a*b;
+                System.out.println(a+"*"+b+"="+ans);
+                break;
+            }
+            case '/':{
+                ans = a/b;
+                System.out.println(a+"/"+b+"="+ans);
+                break;
+            }
+                
+                
+        
+            default:
+                break;
+        }
         
     }
 }
