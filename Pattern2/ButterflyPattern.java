8c8cf723ba36d910d2acc4dcbdbde89a6ad8102c
Mahfuz12309 <mahfuz15-2713@diu.edu.bd>
2024-05-09 15:27:41 +0600

    butterfly, star rombus , zero one triangle code added to the pattern 2

---
+++ Pattern2/ButterflyPattern.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ButterflyPattern {
+    public static void printButterfly(int n)
+    {
+        for(int i=1; i<=n; i++){
+            for(int j=1;j<=i; j++){
+                System.out.print("*");
+            }
+            for(int k=1; k<=2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for(int i=n; i>=1; i--){
+            for(int j=i;j>=1; j--){
+                System.out.print("*");
+            }
+            for(int k=2*(n-i); k>=1;k--){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        printButterfly(n);
+    }
+    
+}
---
+++ Pattern2/StarRombus.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class StarRombus {
+    public static void printRombus(int n)
+    {
+        for(int i=1;i<=n; i++)
+        {
+            for(int k=1;k<=(n-i);k++)
+            {
+                System.out.print("@");
+            }
+            for (int j=1; j<=n; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =  sc.nextInt();
+        printRombus(n);
+        
+    }
+    
+}
