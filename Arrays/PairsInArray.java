e30fa35c837f771ca1b23a33deb5cacd634a92f5
Mahfuz12309 <mahfuz15-2713@diu.edu.bd>
2024-05-29 18:05:54 +0600

    array problem added

---
+++ Arrays/PairsInArray.java
@@ -0,0 +1,21 @@
+public class PairsInArray {
+    public static void pairsinarray(int number[])
+    {
+        for(int i=0;i<=number.length-1;i++)
+        {
+            int curr= number[i];
+            for(int j=i+1;j<=number.length-1;j++)
+        {
+            System.out.print("("+curr+","+number[j]+")");
+            
+        }
+        System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int number[]={2,4,6,8,10};
+        pairsinarray(number);
+    }
+    
+}
