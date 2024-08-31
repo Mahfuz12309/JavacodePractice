109cb5be54b1576a4e448894efbd1238d9c98496
Mahfuz12309 <mahfuz15-2713@diu.edu.bd>
2024-08-31 10:52:36 +0600

    3 code of array prrogram has been added today

---
+++ Arrays/ReverseArray.java
@@ -0,0 +1,25 @@
+public class ReverseArray {
+    public static void reversearray (int num[])
+    {
+        int first = 0;int last=num.length-1;
+        while(first<last){
+            int temp= num[last];
+            num[last]=num[first];
+            num[first]=temp;
+            first++;
+            last--;
+        }
+        
+
+    } 
+    public static void main(String[] args) {
+        int num[] = {2,4,6,8,10,12,14};
+        reversearray(num);
+        for(int i=0;i<num.length;i++)
+        {
+            System.out.print(num[i]+" ");
+        }
+        System.err.println();
+
+    }
+}
---
+++ Arrays/pairInArray.java
@@ -0,0 +1,18 @@
+public class pairInArray {
+    public static void printpair(int num[])
+    {
+        
+        for(int i=0;i<num.length;i++)
+        {
+            for(int j=i+1;j<num.length;j++)
+            {
+                System.out.print("("+num[i]+","+num[j]+") ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int num[]={2,4,6,8,10};
+        printpair(num);
+    }
+}
